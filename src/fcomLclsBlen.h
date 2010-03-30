944843191f3aa8055ca846cd848fd78fe11a99e9
qing <>
2010-03-30 22:55:47 +0000

    Add blen fcom data status codes.

--- src/fcomLclsBlen.h
+++ src/fcomLclsBlen.h
@@ -6,7 +6,7 @@ extern "C" {
 #endif
 
 
-/* Macros to access BLEN array fields; use as follows:
+/* Macros to access BLEN Array fields; use as follows:
  *
  * FcomBlobRef p_blenData;
  *
@@ -19,6 +19,19 @@ extern "C" {
 #define fcbl_blen_braw  fc_flt[2]
 #define fcbl_blen_bimax fc_flt[3]
 
+/* Status values
+ * The IMAX value is invalid when any of the following is true:
+ *  (1) tmit did not arrive
+ *  (2) tmit value is too small
+ *  (3) the raw value is too small
+ * Since there are two imax values, we will use a bitmap for the error code
+ *  bit 0 -- aimax
+ *  bit 1 -- bimax
+ * An error exists when the corresponding bit in the status is set.
+ */
+#define FC_STAT_BLEN_OK                0
+#define FC_STAT_BLEN_INVAL_AIMAX_MASK  1  
+#define FC_STAT_BLEN_INVAL_BIMAX_MASK  2
 
 #ifdef __cplusplus
 }
